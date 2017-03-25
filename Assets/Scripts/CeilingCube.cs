d17e8ae2dbe0e2b1b587f89b27af5dc038b465f3
HOCgaming <harveycash@outlook.com>
2017-03-25 18:04:36 +0000

    CubeCeiling created on rollover

---
+++ Assets/Scripts/CeilingCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingCube : MonoBehaviour {
+
+    [SerializeField] private int health;
+    private int maxHealth = 1000;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+	void Damage(int damage)
+    {
+        health -= damage;
+        gameObject.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.white, health / (float)maxHealth);
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        Damage(1);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private static int maxLength = 9;
+    private static int maxLength = 15;
     private bool[,] ceilingBools = new bool[maxLength, maxLength];
-    private GameObject[,] ceiling = new GameObject[maxLength, maxLength];
+
+    private Transform ceilingParent;
+    private CeilingCube[,] ceiling = new CeilingCube[maxLength, maxLength];
 
     void Start()
     {
+        ceilingParent = new GameObject("Ceiling Parent").transform;
+
         ceilingBools[maxLength / 2, maxLength / 2] = true;
-        BuildCeiling();
+        BuildCeiling(maxLength / 2, maxLength / 2);
     }
 
-    void BuildCeiling()
+    void BuildCeiling(int j, int i)
     {
-        for (int j = 0; j < maxLength; j++)
+        if(ceiling[j, i] == null)
         {
-            for (int i = 0; i < maxLength; i++)
-            {
-                if(ceilingBools[j, i] && ceiling[j, i] == null)
-                {
-                    Debug.Log("making new cube at " + i + ", " + j);
-                    ceiling[j, i] = new GameObject(i + ", " + j);
-                }
-            }
-        }
+            ceiling[j, i] = Instantiate((GameObject)Resources.Load("Prefabs/CeilingCube")).GetComponent<CeilingCube>();
+            ceiling[j, i].transform.position = new Vector3(i - (maxLength / 2), 0, j - (maxLength / 2));
+            ceiling[j, i].transform.parent = ceilingParent;
+        }        
     }
     
 
@@ -39,19 +38,23 @@ public class PlayerController : MonoBehaviour
 
     void CheckInputs()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) {  RollInDirection(Vector3.forward, Vector3.right); }
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) { RollInDirection(Vector3.forward, Vector3.right); }
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) { RollInDirection(Vector3.back, Vector3.left); }
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) { RollInDirection(Vector3.left, Vector3.forward); }
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) { RollInDirection(Vector3.right, Vector3.back); }
     }
-
-    private int initX = maxLength / 2, initY = maxLength / 2;
+    
     void RollInDirection(Vector3 direction, Vector3 axis)
     {
-        Vector3 rotatePoint = transform.position + (0.5f * direction) + (0.5f * Vector3.down);
-        transform.RotateAround(rotatePoint, axis, 90);
-
-        ceilingBools[(int)Mathf.Round(transform.position.z) + (maxLength / 2), (int)Mathf.Round(transform.position.x) + (maxLength / 2)] = true;
-        BuildCeiling();
+        Vector3 nextPos = transform.position + direction;
+        bool withinZBounds = ((int)Mathf.Round(nextPos.z) + (maxLength / 2) > 0 && (int)Mathf.Round(nextPos.z) + (maxLength / 2) < maxLength);
+        bool withinXBounds = ((int)Mathf.Round(nextPos.x) + (maxLength / 2) > 0 && (int)Mathf.Round(nextPos.x) + (maxLength / 2) < maxLength);
+        if(withinXBounds && withinZBounds)
+        {
+            Vector3 rotatePoint = transform.position + (0.5f * direction) + (0.5f * Vector3.down);
+            transform.RotateAround(rotatePoint, axis, 90);
+            
+            BuildCeiling((int)Mathf.Round(transform.position.z) + (maxLength / 2), (int)Mathf.Round(transform.position.x) + (maxLength / 2));
+        }        
     }
 }
